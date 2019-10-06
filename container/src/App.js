eab5f730bb8cc99a294ee5fc14458a326e15ea6f
Umar Muneer <umarmuneer@gmail.com>
2019-10-06 12:33:43 +0500

    initial commit

---
+++ container/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+        <div className="App-content">
+
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ container/src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <a className="element" href="/">
+                    App1
+                </a>
+                <a className="element" href="/">
+                    App2
+                </a>
+            </div>
+        )
+    }
+}
+
+export default Header;
